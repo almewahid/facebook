be41e5acef0b16ce708e5775374ce808af961844
almewahid <osakr100@gmail.com>
2025-12-21 13:09:21 +0200

    Update CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://facebook-ten-lac.vercel.app",
+    "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
