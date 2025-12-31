dede687e35355b3b4bfb5a1dd8a1a6e0ea756bd4
almewahid <osakr100@gmail.com>
2025-12-31 03:14:49 +0200

    Create selenium_bot.py

---
+++ backend/app/bot/selenium_bot.py
@@ -0,0 +1,12 @@
+class FacebookBot:
+    def __init__(self):
+        pass
+    
+    def login(self, email, password):
+        pass
+    
+    def post_to_group(self, group_id, content):
+        pass
+    
+    def logout(self):
+        pass
